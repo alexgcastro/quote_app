0825de61cfe97e12dce02c8aa2564c244d14c23c
Alejandro G. Castro <a.garcia.castro@gmail.com>
2014-03-05 20:14:09 +0100

    Fix style, use 4-space indentation. Still undecided but emacs says it is good.

--- service/quote_server.js
+++ service/quote_server.js
@@ -13,9 +13,9 @@ var id = 1;
 var tempDirname = 'generated_images';
 
 function addHeaders(res) {
-  res.header('Access-Control-Allow-Origin ', '*');
-  res.header('Access-Control-Allow-Method', 'POST, GET, PUT, DELETE, OPTIONS' );
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-File-Name, Content-Type, Cache-Control' );
+    res.header('Access-Control-Allow-Origin ', '*');
+    res.header('Access-Control-Allow-Method', 'POST, GET, PUT, DELETE, OPTIONS' );
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-File-Name, Content-Type, Cache-Control' );
 }
 
 function removeById(id)
@@ -31,10 +31,10 @@ function removeById(id)
 
 function log_error(error, stdout, stderr)
 {
-  if (stdout) console.log('pango-view_stdout: ' + stdout);
-  if (stderr) console.log('pango-view_stderr: ' + stderr);
-  if (error !== null)
-      console.log('node-pango-view error: ' + error);
+    if (stdout) console.log('pango-view_stdout: ' + stdout);
+    if (stderr) console.log('pango-view_stderr: ' + stderr);
+    if (error !== null)
+        console.log('node-pango-view error: ' + error);
 }
 
 function generateImage(res, model)
@@ -55,42 +55,42 @@ function generateImage(res, model)
 function respond(req, res, next)
 {
 
-  addHeaders(res);
+    addHeaders(res);
 
-  if ('HEAD' == req.method) {
-      res.send(200, 'OK');
-      return;
-  }
+    if ('HEAD' == req.method) {
+        res.send(200, 'OK');
+        return;
+    }
 
-  if ('OPTIONS' == req.method) {
-      res.send(203, 'OK');
-      return;
-  }
+    if ('OPTIONS' == req.method) {
+        res.send(203, 'OK');
+        return;
+    }
 
-  if ('DELETE' == req.method) {
-      console.log('Deleting %s!', req.params.id);
-      removeById(req.params.id);
-      res.send(200, 'OK');
-      return;
-  }
+    if ('DELETE' == req.method) {
+        console.log('Deleting %s!', req.params.id);
+        removeById(req.params.id);
+        res.send(200, 'OK');
+        return;
+    }
 
-  if (req.isUpload()) {
-      req.on('data', function (data) {
-          var model = JSON.parse(data);
-          model.id = id++;
-          Quotes.push(model);
-          console.log('Received quote: ' + model.id + ' ' + model.text + ' ' + model.author);
-          generateImage(res, model);
-      });
-      return;
-  }
+    if (req.isUpload()) {
+        req.on('data', function (data) {
+            var model = JSON.parse(data);
+            model.id = id++;
+            Quotes.push(model);
+            console.log('Received quote: ' + model.id + ' ' + model.text + ' ' + model.author);
+            generateImage(res, model);
+        });
+        return;
+    }
 
-  if (req.params.id != undefined)
-      res.send(JSON.stringify(Quotes[req.params.id]));
-  else if (Quotes.length > 0)
-      res.send(JSON.stringify(Quotes));
+    if (req.params.id != undefined)
+        res.send(JSON.stringify(Quotes[req.params.id]));
+    else if (Quotes.length > 0)
+        res.send(JSON.stringify(Quotes));
 
-  res.send(200, '');
+    res.send(200, '');
 }
 
 server.head('/quote', respond);
@@ -105,5 +105,5 @@ server.get(/\/generated_images\/?.*/, restify.serveStatic({
 }));
 
 server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+    console.log('%s listening at %s', server.name, server.url);
 });
