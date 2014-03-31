bd0523de5d1f839bfe30010e55f07cb6c8637a68
Alejandro G. Castro <a.garcia.castro@gmail.com>
2014-03-31 20:01:59 +0200

    Serve the ui using node instead of using the file.
    This seems more natural for testing and we avoid the cross origin
    cruft in the code.

--- app/quote_service.js
+++ app/quote_service.js
@@ -11,6 +11,7 @@ var id = 1;
 
 // Defines.
 var tempDirname = '/generated_images';
+var uiDirname = '/ui';
 
 function generateImage(res, model)
 {
@@ -50,22 +51,10 @@ function respond(req, res, next)
     res.send(200, 'OK');
 }
 
-var enableCORS = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    if ('OPTIONS' == req.method)
-        res.send(203, 'OK');
-    else
-        next();
-};
-
-app.use(enableCORS);
-
 app.head('/quote', respond);
 app.post('/quote', respond);
 
+app.use(express.static(__dirname + uiDirname));
 app.use(tempDirname, express.static(__dirname + tempDirname));
 
 app.set('title', 'Quote app');
