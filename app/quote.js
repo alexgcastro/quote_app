4e2ba09f10b97aaba82ca0df80b1a551e7b371ca
Alejandro G. Castro <a.garcia.castro@gmail.com>
2014-04-11 13:49:03 +0200

    Remove the AppView variable.
    This was the last backbone view class, we were not using it. Now
    the react component handles its own update.

--- app/quote.js
+++ app/quote.js
@@ -1,7 +1,6 @@
 require(['jquery', 'backbone', 'react'], function($, Backbone, React) {
 
     var Quote = Backbone.Model.extend({
-
         defaults: function() {
             return {
                 text: null,
@@ -13,67 +12,55 @@ require(['jquery', 'backbone', 'react'], function($, Backbone, React) {
     });
 
     var QuoteList = Backbone.Collection.extend({
-
         model: Quote,
-
         url: 'http://localhost:8080/quote',
-
         comparator: 'id'
-
     });
 
-    var Quotes = new QuoteList;
+    var quotes = new QuoteList;
 
     var QuoteView = React.createClass({
-        render: function () {
+        getInitialState: function() {
+            this.text = $("#text");
+            this.author = $("#author");
+            this.main = $('#main');
+
+            $("#text").bind("keypress", this.createQuote);
+            $("#author").bind("keypress", this.createQuote);
+
+            quotes.listenTo(quotes, 'add', this.doRender);
+            quotes.listenTo(quotes, 'change', this.doRender);
+
+            return {quote: null};
+        },
+
+        render: function() {
             if (!this.props.quote)
                 return <div> </div>;
 
             var quote = this.props.quote;
 
             return <img src={quote.image} alt={quote.text+" -- "+quote.author}></img>;
-        }
-    });
-
-    var AppView = Backbone.View.extend({
-
-        el: $("#quoteapp"),
-
-        events: {
-            "keypress #text":  "createOnEnter",
-            "keypress #author":  "createOnEnter",
         },
 
-        initialize: function() {
-
-            this.text = this.$("#text");
-            this.author = this.$("#author");
-            this.image = this.$("#image");
-
-            this.listenTo(Quotes, 'add', this.renderQuote);
-            this.listenTo(Quotes, 'change', this.renderQuote);
-
-            this.main = $('#main');
-        },
-
-        renderQuote: function(quote) {
+        doRender: function(quote) {
             React.renderComponent(<QuoteView quote={_.clone(quote.attributes)} />, this.main.get(0));
         },
 
-        createOnEnter: function(key) {
+        createQuote: function(key) {
             if (key.keyCode != 13) return;
             if (!this.text.val() || (!this.author.val())) return;
 
-            var oldQuote = Quotes.pop();
+            var oldQuote = quotes.pop();
             if (oldQuote) oldQuote.trigger('destroy');
 
-            Quotes.create({text: this.text.val(), author: this.author.val(), template: 2});
+            quotes.create({text: this.text.val(), author: this.author.val(), template: 2});
 
             this.text.val('');
             this.author.val('');
         }
+
     });
 
-    var App = new AppView;
-
+    React.renderComponent(<QuoteView />, $('#main').get(0));
 });
